a10cc241d2b5c65af29c96e812f1f659bdbc78a3
CASY82 <repaid11010@gmail.com>
2021-11-10 21:44:58 +0900

    에러사항 수정 및 들여쓰기 맞춤

--- learn_react/src/components/Subject.js
+++ learn_react/src/components/Subject.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 class Subject extends Component {
     render(){
--- learn_react/src/components/TOC.js
+++ learn_react/src/components/TOC.js
@@ -5,9 +5,9 @@ class TOC extends Component{
       return (
         <nav>
           <ul>
-              <li><a href="1.html">HTML</a></li>
-              <li><a href="2.html">CSS</a></li>
-              <li><a href="3.html">JavaScript</a></li>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
           </ul>
         </nav>
       );

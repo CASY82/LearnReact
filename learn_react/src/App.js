a5eba098a11411b8d9c1edc2c4f0cb45b5cbfe98
CASY82 <repaid11010@gmail.com>
2021-11-11 17:41:28 +0900

    17강 컴포넌트 이벤트

--- learn_react/src/App.js
+++ learn_react/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      mode:'read',
+      mode:'welcome',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
@@ -26,26 +27,39 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break
+        }
+        i = i + 1;
+      }
     }
 
     console.log("render", this);
 
     return (
       <div className="App">
-        {/* <Subject 
+        <Subject 
           title={this.state.subject.title} 
-          sub={this.state.subject.sub}></Subject> */}
-        <header>
-          <h1><a href="/" onClick={function (e){
-            console.log("event in", this);
-            e.preventDefault();
-            this.setState({ mode: "welcome" });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>
-        <TOC data={this.state.contents}></TOC>
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+          ></Subject>
+        <TOC 
+          data={this.state.contents}
+          onChangePage={function(id){
+            // debugger;
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+          });
+          }.bind(this)}
+          ></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );

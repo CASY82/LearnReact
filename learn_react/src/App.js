535f8d97451adfc8bbbaacd97379b406abf5d256
CASY82 <repaid11010@gmail.com>
2022-01-05 01:44:02 +0900

    19_Create 구현 (p150)

--- learn_react/src/App.js
+++ learn_react/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
 import Subject from './components/Subject';
 import Control from './components/Control';
+import CreateContent from './components/CreateContent';
 
 class App extends Component {
   constructor(props){
@@ -23,10 +24,11 @@ class App extends Component {
 
   render(){
     console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read'){
       var i = 0;
       while(i < this.state.contents.length) {
@@ -38,6 +40,11 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        console.log(_title, _desc);
+      }.bind(this)}></CreateContent>
     }
 
     console.log("render", this);
@@ -66,7 +73,7 @@ class App extends Component {
               mode : _mode
           });
         }.bind(this)}></Control>
-        <Content title={_title} desc={_desc}></Content>
+        {_article}
       </div>
     );
   }
